32364359a69f6167d268b0e2ef5cb1457a89fb9f
xuelwang7 <wan22008@byui.edu>
2023-01-07 22:54:06 -0700

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score?");
+        string userinput = Console.ReadLine();
+        int n = int.Parse(userinput);
+        
+        string letter = "";
+
+        if (n >= 90)
+        {
+            letter ="A";
+        }
+        else if (n >= 80)
+        {
+            letter ="B";
+        }
+        else if (n >= 70)
+        {
+            letter ="C";
+        }
+        else if (n >= 60)
+        {
+            letter ="D";
+        }
+        else 
+        {
+            letter ="F";
+        }
+
+        Console.WriteLine($"Your letter is {letter}");
+        
+        Console.Write("what is your letter grade?");
+        string grade = Console.ReadLine();
+        if (grade =="A" || grade == "B" || grade == "C")
+        {
+            Console.WriteLine("Congratulation! You pass the class!");
+        }
+        else
+        {
+            Console.WriteLine("Take it again.");
+        }
+
+
     }
 }
