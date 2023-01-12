c32ebea27334a7727123e94bf800ff47d9036730
xuelwang7 <wan22008@byui.edu>
2023-01-11 20:55:22 -0700

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        int userinput = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+    
+        bool guessed = false ;
+        int turns = 0;
+        while (guessed == false)
+        {    
+            Console.Write("please enter your guess:");
+            int answer = int.Parse(Console.ReadLine());
+            turns += 1;
+
+            if (answer > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(answer < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Great!");
+                guessed = true;
+            }
+        }
+        Console.WriteLine($"it tooks you {turns} turns to guess the number.");
+        
+
     }
 }
